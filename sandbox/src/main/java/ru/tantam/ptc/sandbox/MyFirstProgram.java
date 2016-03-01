a065c10e229e33f4ac7dae2a401347b3b6c84c78
tantamsh <tantamsh@gmail.com>
2016-03-01 23:09:16 +0600

    Добавлено несколько методов

--- sandbox/src/main/java/ru/tantam/ptc/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/tantam/ptc/sandbox/MyFirstProgram.java
@@ -1,7 +1,28 @@
 package ru.tantam.ptc.sandbox;
 
 public class MyFirstProgram {
-	public static void main(String[] arg){	
-		System.out.println("Hello,world!");
+
+  public static void main(String[] arg){
+		hello("user");
+    hello("world");
+    double l=8.0;
+    System.out.println("Площадь квадрата со стороной " + l  + " = "+ area(l));
+
+    double a=6;
+    double b=4;
+    System.out.println("Площадь прямоугольника со сторонами " + a + " и " + b + " = "+ area(a,b));
 	}
+
+  public static void hello(String somebody){
+    System.out.println("Hello, " + somebody + "!");
+  }
+
+  public static double area(double l){
+    return l*l;
+  }
+
+  public static double area(double a, double b){
+    return a*b;
+  }
+
 }
