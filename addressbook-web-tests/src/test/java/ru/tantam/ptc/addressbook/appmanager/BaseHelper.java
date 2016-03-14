6b3a1608f960afb3162fdb648376afce9359e2f1
tantamsh <tantamsh@gmail.com>
2016-03-14 23:28:55 +0600

    Добавлена возможность запуска тестов в Chrome и IE

--- addressbook-web-tests/src/test/java/ru/tantam/ptc/addressbook/appmanager/BaseHelper.java
+++ addressbook-web-tests/src/test/java/ru/tantam/ptc/addressbook/appmanager/BaseHelper.java
@@ -2,15 +2,16 @@ package ru.tantam.ptc.addressbook.appmanager;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.NoAlertPresentException;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 /**
  * Created by Tanya on 09.03.2016.
  */
 public class BaseHelper {
-  protected FirefoxDriver wd;
+  protected WebDriver wd;
 
-  public BaseHelper(FirefoxDriver wd) {
+  public BaseHelper(WebDriver wd) {
     this.wd = wd;
   }
 
--- addressbook-web-tests/src/test/java/ru/tantam/ptc/addressbook/appmanager/GroupHelper.java
+++ addressbook-web-tests/src/test/java/ru/tantam/ptc/addressbook/appmanager/GroupHelper.java
@@ -1,6 +1,7 @@
 package ru.tantam.ptc.addressbook.appmanager;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import ru.tantam.ptc.addressbook.model.GroupData;
 
@@ -9,7 +10,7 @@ import ru.tantam.ptc.addressbook.model.GroupData;
  */
 public class GroupHelper extends BaseHelper {
 
-  public GroupHelper(FirefoxDriver wd) {
+  public GroupHelper(WebDriver wd) {
     super(wd);
   }
 
