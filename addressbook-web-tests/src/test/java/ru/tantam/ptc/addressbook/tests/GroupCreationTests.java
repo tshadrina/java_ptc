2f39e67853b414132578cf6b6f1ac785ff731bec
tantamsh <tantamsh@gmail.com>
2016-03-23 13:20:41 +0600

    Тест на создание группы сравнивает списки групп до и после создания

--- addressbook-web-tests/src/test/java/ru/tantam/ptc/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/tantam/ptc/addressbook/tests/GroupCreationTests.java
@@ -4,6 +4,7 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.tantam.ptc.addressbook.model.GroupData;
 
+import java.util.HashSet;
 import java.util.List;
 
 
@@ -13,10 +14,21 @@ public class GroupCreationTests extends BaseTest {
   public void testGroupCreation() {
     app.getNavigationHelper().gotoGroupPage();
     List<GroupData> before = app.getGroupHelper().getGroupList();
-    app.getGroupHelper().createGroup(new GroupData("test2", "test2_header", "test2_footer"));
+    GroupData group = new GroupData("test2", "test2_header", "test2_footer");
+    app.getGroupHelper().createGroup(group);
     List<GroupData> after = app.getGroupHelper().getGroupList();
     Assert.assertEquals(after.size(), before.size() + 1);
 
+    int max = 0;
+    for (GroupData g: after){
+      if (g.getId() > max){
+        max = g.getId();
+      }
+    }
+    group.setId(max);
+    before.add(group);
+
+    Assert.assertEquals(new HashSet<Object>(after), new HashSet<Object>(before));
   }
 
 }
