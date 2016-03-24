c8c8854786e0a018f0feb64077e79cfdb8c4719f
tantamsh <tantamsh@gmail.com>
2016-03-24 15:15:30 +0600

    Проверки через сравнение упорядоченных списков
    в тестах создания и модификации групп

--- addressbook-web-tests/src/test/java/ru/tantam/ptc/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/tantam/ptc/addressbook/tests/GroupCreationTests.java
@@ -4,6 +4,7 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.tantam.ptc.addressbook.model.GroupData;
 
+import java.util.Comparator;
 import java.util.HashSet;
 import java.util.List;
 
@@ -18,10 +19,17 @@ public class GroupCreationTests extends BaseTest {
     app.getGroupHelper().createGroup(group);
     List<GroupData> after = app.getGroupHelper().getGroupList();
     Assert.assertEquals(after.size(), before.size() + 1);
+    
+//    group.setId(after.stream().max((o1, o2) -> Integer.compare(o1.getId(),o2.getId())).get().getId());
+//    before.add(group);
+//    Assert.assertEquals(new HashSet<Object>(after), new HashSet<Object>(before));
 
-    group.setId(after.stream().max((o1, o2) -> Integer.compare(o1.getId(),o2.getId())).get().getId());
+    Comparator<? super GroupData> byId = (g1, g2) -> Integer.compare(g1.getId(), g2.getId());
+    group.setId(after.stream().max(byId).get().getId());
     before.add(group);
-    Assert.assertEquals(new HashSet<Object>(after), new HashSet<Object>(before));
+    before.sort(byId);
+    after.sort(byId);
+    Assert.assertEquals(after, before);
   }
 
 }
